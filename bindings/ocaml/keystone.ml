ade658c24f0ac239f865a75a1c6f1fb55ff77566
Aziem Chawdhary <aziem@chawdhary.co.uk>
2016-06-16 12:42:58 +0100

    Started on documentation of API.

--- bindings/ocaml/keystone.ml
+++ bindings/ocaml/keystone.ml
@@ -15,21 +15,33 @@ open B
 type asm_result =
   | ASMSuccess of (int array) * int * int
   | ASMError of string
- 
+
+(** Result of opening an assembly engine. 
+    KSOpenSucc indicates success and contains (a C pointer) to the engine. 
+    KSOpenError indicates an error and contains the error string.  
+*)
 type ks_open_result =
   | KSOpenSucc of ks_struct structure ptr
   | KSOpenError of string
 
+
+(** Determin if the given architecture is supported. True if the
+    architecture is supported, false otherwise *)
 let ks_arch_supported arch =
   ks_arch_supported_ arch
 
+(** Returns a combined API version *)                     
 let ks_version major minor =
   let major = Unsigned.UInt.of_int major in
   let minor = Unsigned.UInt.of_int minor in
   let u = ks_version_ (allocate uint major) (allocate uint minor) in
   Unsigned.UInt.to_int u
                       
+(** Set an option for a Keystone engine instance. 
+    Takes an engine instance, an option type and an option value. 
 
+    Returns KS_ERR_OK on success or another value on failure. 
+*)
 (* TODO: better handling of error value *)
 let ks_option engine opttype optvalue =
   match opttype with
@@ -38,6 +50,11 @@ let ks_option engine opttype optvalue =
 let ks_strerror err =
   ks_strerror_ err
 
+(** Create a new instance of Keystone engine. 
+    arch: is architecture type, mode is hardware mode. 
+    
+    Returns a Keystone engine 
+*)
 let ks_open arch ?(endian=KS_MODE_LITTLE_ENDIAN) mode =
   let mode =
     match endian with
@@ -57,11 +74,22 @@ let ks_open arch ?(endian=KS_MODE_LITTLE_ENDIAN) mode =
   | T.KS_ERR_OK -> KSOpenSucc (!@ engine)
   | _ as err -> KSOpenError(ks_strerror err)
                            
-     
+
+(** Close the Keystone engine instance *)
 let ks_close engine = ks_close_ engine 
 
+(** Given a Keystone engine instance, returns the last error number on
+    some API function failure. *)
 let ks_errno engine = ks_err_ engine
-                         
+
+
+(** Assemble a string: takes a Keystone engine instance, a string to
+assember and a start address to assemble from. 
+
+On success returns ASMSuccess with an array of the encoded input
+assembly string, the size of the encoding and the number of statements
+successfully processed.
+ *)
 let ks_asm engine str addr =
   let addr1 = Unsigned.UInt64.of_int addr in
   let addr2 = Unsigned.UInt64.to_int64 addr1 in
@@ -80,6 +108,7 @@ let ks_asm engine str addr =
   | _ -> let err = ks_errno engine in
          ASMError(ks_strerror err)
          
-          
+(** Convert an array containing an encoding of an assembly string and
+returns a string representation. *)
 let asm_array_to_string a =
   Array.fold_left (fun str c -> let t = Printf.sprintf "%02x " c in str^t) "" a 
